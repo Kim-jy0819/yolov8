47c0b941ee0b8e150223875388e6e6e8d7a70eee
jinyoung <charlie0819@naver.com>
2023-04-22 16:58:25 +0900

    inference code

--- inference.py
+++ inference.py
@@ -11,6 +11,7 @@ from collections import Counter
 from tqdm import tqdm
 from ultralytics import YOLO
 
+
 class CustomDataset(Dataset):
     def __init__(self, annotation, data_dir):
         super().__init__()
@@ -29,7 +30,7 @@ class CustomDataset(Dataset):
         return len(self.coco.getImgIds())
     
 # 이미지의 예측된 box 얻어서 submission file 내용 만들기
-def get_bboxes(annotation, data_loader, model, iou_threshold, threshold, pred_format="cells", box_format="midpoint", device="cuda"):
+def get_bboxes(annotation, data_loader, model):
 
     
     # submission 파일에 저장될 내용
@@ -37,28 +38,28 @@ def get_bboxes(annotation, data_loader, model, iou_threshold, threshold, pred_fo
     file_names = [] 
     
     coco = COCO(annotation)
-    for batch_idx, file_names in enumerate(tqdm(data_loader)):
+    for batch_idx, image_batch in enumerate(tqdm(data_loader)):
         # 이미지에서 예측한 모든 박스들
         all_pred_boxes = []
         
         #이미지 정보
-        image_info = coco.loadImgs(coco.getImgIds(imgIds=batch_idx))[0]
         
         with torch.no_grad():
-            predictions = model(file_names)
-        batch_size = len(file_names)
+            predictions = model(image_batch)
+        batch_size = len(image_batch)
         for idx in range(batch_size):
+            file_name = osp.join('test', osp.basename(image_batch[idx]))
             pred_info = predictions[idx].boxes
-            classes = pred_info.cls
-            bboxes = pred_info.xywh
-            conf = pred_info.conf
+            classes = pred_info.cls.tolist()
+            bboxes = pred_info.xyxy.tolist()
+            conf = pred_info.conf.tolist()
 
             # submission 파일에 저장될 내용
             prediction_string = ''
             for pred_class, conf, box in zip(classes, conf, bboxes):
                 prediction_string += str(int(pred_class))+ ' ' + str(conf) + ' ' + str(box[0]) + ' ' + str(box[1]) + ' ' + str(box[2]) + ' ' + str(box[3]) + ' '
             prediction_strings.append(prediction_string)
-            file_names.append(image_info['file_name'])
+            file_names.append(file_name)
  
     return prediction_strings, file_names
 
@@ -68,7 +69,7 @@ torch.manual_seed(seed)
 
 # Hyperparameters etc. 
 DEVICE = "cuda" if torch.cuda.is_available else "cpu" 
-LOAD_MODEL_FILE = "/root/jinyoung/ultralytics/runs/detect/train3/weights/best.pt" # inference에 사용할 model
+LOAD_MODEL_FILE = "/root/jinyoung/ultralytics/runs/detect/train5/weights/best.pt" # inference에 사용할 model
 
 
 def inference():
@@ -84,13 +85,13 @@ def inference():
     test_dataset = CustomDataset(annotation, data_dir)
     test_data_loader = DataLoader(
         test_dataset,
-        batch_size=5000, # only batch_size ==1 support !!!
+        batch_size=8, 
         shuffle=False,
         num_workers=4
     )  
     # 예측 및 submission 파일 생성
     prediction_strings, file_names = get_bboxes(
-        annotation, test_data_loader, model, iou_threshold=0.5, threshold=0.4
+        annotation, test_data_loader, model
     )
     submission = pd.DataFrame()
     submission['PredictionString'] = prediction_strings
