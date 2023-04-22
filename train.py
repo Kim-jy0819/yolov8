ba50c21d618aff782c628118f048311fc1ff32bf
jinyoung <charlie0819@naver.com>
2023-04-22 16:58:51 +0900

    train code

--- train.py
+++ train.py
@@ -4,7 +4,4 @@ from ultralytics import YOLO
 # model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt") 
 # Use the model
-model.train(data="recycle.yaml", epochs=3)  # train the model
-metrics = model.val()  # evaluate model performance on the validation set
-results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
-success = model.export(format="onnx")  # export the model to ONNX format
+model.train(data="recycle.yaml")  # train the model
